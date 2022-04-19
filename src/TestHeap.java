3219bf5d7dc4ea04c7603c35b6229c0b10c333d0
Minh Tran <mtran8011@yahoo.com>
2022-04-18 21:36:18 -0700

    Added .gitignore, input files, and cleaned up TestHeap

--- src/TestHeap.java
+++ src/TestHeap.java
@@ -7,71 +7,82 @@ public class TestHeap
 {
 
         public static void main(String[] args) 
-        {              
-               Integer a[] = new Integer [100];
-               
-               //Input the data from the file 
-                try {
-                        int i=0;
-                        File file = new File("data_random.txt");
-                        Scanner scanner = new Scanner(file);
-                        while (scanner.hasNextLine())
-                        {
-                                String num = scanner.nextLine(); //Input data from each line of the file
-                                a[i] = Integer.parseInt(num);
-                                i++;
-                        }
-                        scanner.close();
-                 } 
-                catch (FileNotFoundException e) {
-                        System.out.println("The file does not exist.");
-                        e.printStackTrace();
-                }
+        {
+            File file1 = new File("data_random.txt");
+            File file2 = new File("data_sorted.txt");
 
-                //write the data to the output file        
-                try {
-                        //open a file to write data
-                        PrintWriter f =new PrintWriter("output.txt");
-                        MaxHeap <Integer> newheap = new MaxHeap<>(a);
-                       
-                        //Optimal method
-                        newheap.optimalMethod(a);
-                        f.write("Heap built using optimal method: ");  
-                        
-                        //Print out the heap
-                        for(int i = 1; i <= 10; i++)
-                        {
-                                f.write(newheap.getData(i) + ", ");
-                        }
-                        
-                        f.write("...\n");
+            outputMaxHeapWithOptimalMethod(file1);
+        }
 
-                        //Print out the number of swaps
-                        f.write("Number of swaps in the heap creation: " + newheap.getSwap() + "\n" );
+        private static void outputMaxHeapWithSequentialInsertion(File givenFile)
+        {
 
-                        //Removeals on heap
-                        for(int i = 1; i<= 10; i++)
-                        {
-                                newheap.removeMax();
-                                
-                        }
+        }
 
-                        //Print out the heap after 10 removal
-                        f.write("Heap after 10 removals: ");
-                        for(int i = 1; i <= 10; i++)
-                        {
-                                f.write(newheap.getData(i) + ", ");
-                        }
-                        f.write("...\n");                        
+        private static void outputMaxHeapWithOptimalMethod(File givenFile)
+        {
+            Integer a[] = new Integer [100];
 
-                        f.close();
-
-
-               } 
-                catch(FileNotFoundException e)
+            //Input the data from the file
+            try {
+                int i=0;
+                File file = givenFile;
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine())
                 {
-                        System.out.println("Cannot write data to the file");
+                    String num = scanner.nextLine(); //Input data from each line of the file
+                    a[i] = Integer.parseInt(num);
+                    i++;
                 }
-       
+                scanner.close();
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("The file does not exist.");
+                e.printStackTrace();
+            }
+
+            //write the data to the output file
+            try {
+                //open a file to write data
+                PrintWriter f = new PrintWriter("output.txt");
+                MaxHeap <Integer> newheap = new MaxHeap<>(a);
+
+                //Optimal method
+                newheap.optimalMethod(a);
+                f.write("Heap built using optimal method: ");
+
+                //Print out the heap
+                for(int i = 1; i <= 10; i++)
+                {
+                    f.write(newheap.getData(i) + ", ");
+                }
+
+                f.write("...\n");
+
+                //Print out the number of swaps
+                f.write("Number of swaps in the heap creation: " + newheap.getSwap() + "\n" );
+
+                //Removeals on heap
+                for(int i = 1; i<= 10; i++)
+                {
+                    newheap.removeMax();
+                }
+
+                //Print out the heap after 10 removal
+                f.write("Heap after 10 removals: ");
+                for(int i = 1; i <= 10; i++)
+                {
+                    f.write(newheap.getData(i) + ", ");
+                }
+                f.write("...\n");
+
+                f.close();
+
+
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println("Cannot write data to the file");
+            }
         }
 }
